73098b5d43e390c0f27858ac2abb2159e4366b0b
b8xu <106464777+b8xu@users.noreply.github.com>
2024-11-17 20:23:59 -0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: "/benhxu.github.io/",
   plugins: [react()],
-<<<<<<< HEAD
-=======
   base: "/benhxu.github.io/",
->>>>>>> 2628a6a353b2bd35ca030360f526cc2e4099e072
   assetsInclude: ['**/*.glb', '**/*.gltf'],
 })
