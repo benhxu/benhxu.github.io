033be220d5eb53a838a4b9f4d3d4b561c7c32943
b8xu <106464777+b8xu@users.noreply.github.com>
2024-11-17 19:31:38 -0800

    Set up GitHub Actions for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/<https://benhxu.github.io/>/',
+  
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
 })
