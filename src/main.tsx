b40b018772eb763eee659b87037932852b63a77a
Ben Xu <benxu@Bens-MacBook-Pro.local>
2024-12-15 00:19:52 -0800

    Quick update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+// Ensure TypeScript knows rootElement is not `null`
+if (rootElement) {
+  ReactDOM.createRoot(rootElement as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
